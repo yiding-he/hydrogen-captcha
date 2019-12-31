1acb3a53fedb444f3bef0b91fdba3307e7c7a6f3
Yiding He <yiding.he@gmail.com>
2020-01-01 01:06:19 +0800

    init code

--- src/main/java/com/hyd/captcha/CaptchaGenerator.java
+++ src/main/java/com/hyd/captcha/CaptchaGenerator.java
@@ -2,26 +2,34 @@ package com.hyd.captcha;
 
 import java.awt.*;
 import java.awt.font.FontRenderContext;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import sun.swing.SwingUtilities2;
+import java.util.ArrayList;
 
 public class CaptchaGenerator {
 
+    private static final ThreadLocal<java.util.List<BufferedImage>> CHAR_IMAGE_LIST = new ThreadLocal<>();
+
+    public static java.util.List<BufferedImage> getCharImages() {
+        return CHAR_IMAGE_LIST.get();
+    }
+
     public BufferedImage generate(int width, int height, String content) {
-        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         char[] chars = content.toCharArray();
 
         int cellWidth = width / chars.length;
-        int charWidth = cellWidth * 2;
-        Font font = new Font("DialogInput", Font.ITALIC, charWidth);
+        int charWidth = cellWidth * 3;
+        Font font = new Font("Impact", Font.PLAIN, charWidth);
 
         FontRenderContext frc = new FontRenderContext(null, false, false);
-        Graphics g = bufferedImage.getGraphics();
-        g.setColor(Color.WHITE);
+        Graphics2D g = (Graphics2D) bufferedImage.getGraphics();
+        g.setComposite(AlphaComposite.Clear);
         g.fillRect(0, 0, width, height);
+        g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
         g.setColor(Color.BLACK);
+
+        java.util.List<BufferedImage> charImages = new ArrayList<>();
 
         for (int i = 0; i < chars.length; i++) {
             char aChar = chars[i];
@@ -29,25 +37,37 @@ public class CaptchaGenerator {
             BufferedImage charImage = generateCharImage(aChar, font, frc);
 
             int x = cellWidth * i;
-            int y = height - ((height - charImage.getHeight()) / 2);
-            g.drawImage(charImage, x, y, null);
+            int y = (height - charImage.getHeight()) / 2;
+            g.drawImage(charImage, x, y, charImage.getWidth(), charImage.getHeight(), null);
+
+            charImages.add(charImage);
         }
 
+        CHAR_IMAGE_LIST.set(charImages);
         return bufferedImage;
     }
 
     private BufferedImage generateCharImage(char aChar, Font font, FontRenderContext frc) {
         char[] aCharArr = {aChar};
+
         Rectangle2D bounds = font.getStringBounds(aCharArr, 0, 1, frc);
         BufferedImage image = new BufferedImage(
-            (int) bounds.getWidth(), (int)bounds.getHeight(), BufferedImage.TYPE_INT_RGB
+                (int) bounds.getWidth(), (int)bounds.getHeight(), BufferedImage.TYPE_INT_ARGB
         );
-        Graphics g = image.getGraphics();
-        g.setColor(Color.WHITE);
+
+        Graphics2D g = (Graphics2D) image.getGraphics();
+        FontMetrics fm   = g.getFontMetrics(font);
+        int y = fm.getAscent();
+
+        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+        g.setComposite(AlphaComposite.Clear);
         g.fillRect(0, 0, image.getWidth(), image.getHeight());
+        g.setComposite(AlphaComposite.Src);
         g.setColor(Color.BLACK);
         g.setFont(font);
-        g.drawChars(aCharArr, 0, 1, 0, image.getHeight());
+        g.drawChars(aCharArr, 0, 1, 0, y);
+        g.drawRect(0, 0, image.getWidth() - 1, image.getHeight() - 1);
         return image;
     }
 }
--- src/test/java/com/hyd/captcha/CaptchaGeneratorTest.java
+++ src/test/java/com/hyd/captcha/CaptchaGeneratorTest.java
@@ -1,22 +1,33 @@
 package com.hyd.captcha;
 
-import java.awt.BorderLayout;
-import java.awt.image.BufferedImage;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
 
 public class CaptchaGeneratorTest extends JFrame {
 
     public static void main(String[] args) {
         CaptchaGenerator captchaGenerator = new CaptchaGenerator();
         BufferedImage bufferedImage = captchaGenerator.generate(100, 40, "Styles");
+        List<BufferedImage> charImages = CaptchaGenerator.getCharImages();
 
         CaptchaGeneratorTest frame = new CaptchaGeneratorTest();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
         frame.getContentPane().setLayout(new BorderLayout());
-        frame.getContentPane().add(new JLabel(new ImageIcon(bufferedImage)), BorderLayout.CENTER);
+        frame.getContentPane().add(imageLabel(bufferedImage), BorderLayout.CENTER);
+
+        JPanel charImagePanel = new JPanel();
+        charImagePanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+        charImages.forEach(charImage -> charImagePanel.add(imageLabel(charImage)));
+        frame.getContentPane().add(charImagePanel, BorderLayout.NORTH);
 
         frame.setVisible(true);
+    }
+
+    private static JLabel imageLabel(BufferedImage bufferedImage) {
+        return new JLabel(new ImageIcon(bufferedImage));
     }
 }
