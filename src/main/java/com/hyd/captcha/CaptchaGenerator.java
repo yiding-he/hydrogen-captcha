1b3119f19e4228bc050fe4b0a96f4a1900cc9f6c
He Yiding <yiding.he@gmail.com>
2020-01-02 21:09:17 +0800

    init project

--- src/main/java/com/hyd/captcha/CaptchaGenerator.java
+++ src/main/java/com/hyd/captcha/CaptchaGenerator.java
@@ -2,13 +2,18 @@ package com.hyd.captcha;
 
 import java.awt.*;
 import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.security.SecureRandom;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class CaptchaGenerator {
 
     private static final ThreadLocal<java.util.List<BufferedImage>> CHAR_IMAGE_LIST = new ThreadLocal<>();
+
+    private static final Random RANDOM = new SecureRandom();
 
     public static java.util.List<BufferedImage> getCharImages() {
         return CHAR_IMAGE_LIST.get();
@@ -18,26 +23,33 @@ public class CaptchaGenerator {
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         char[] chars = content.toCharArray();
 
-        int cellWidth = width / chars.length;
-        int charWidth = cellWidth * 3;
+        int cellWidth = width / (chars.length + 1);
+        int charWidth = (int) (height * 0.5);
         Font font = new Font("Impact", Font.PLAIN, charWidth);
 
         FontRenderContext frc = new FontRenderContext(null, false, false);
         Graphics2D g = (Graphics2D) bufferedImage.getGraphics();
-        g.setComposite(AlphaComposite.Clear);
+        g.setColor(Color.WHITE);
         g.fillRect(0, 0, width, height);
         g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER));
-        g.setColor(Color.BLACK);
 
         java.util.List<BufferedImage> charImages = new ArrayList<>();
+        int lastX = -4;
 
         for (int i = 0; i < chars.length; i++) {
             char aChar = chars[i];
 
             BufferedImage charImage = generateCharImage(aChar, font, frc);
 
-            int x = cellWidth * i;
+            int x = (int) (cellWidth * (i + 0.5));
             int y = (height - charImage.getHeight()) / 2;
+
+            x += RANDOM.nextInt(cellWidth) - (cellWidth / 2);
+            y += random(-y + 1, height - charImage.getHeight() - 1 - y);
+
+            x = Math.max(x, lastX + (cellWidth * 2 / 3));
+            lastX = x;
+
             g.drawImage(charImage, x, y, charImage.getWidth(), charImage.getHeight(), null);
 
             charImages.add(charImage);
@@ -47,27 +59,33 @@ public class CaptchaGenerator {
         return bufferedImage;
     }
 
+    private int random(int min, int max) {
+        return RANDOM.nextInt(max - min + 1) + min;
+    }
+
     private BufferedImage generateCharImage(char aChar, Font font, FontRenderContext frc) {
         char[] aCharArr = {aChar};
 
-        Rectangle2D bounds = font.getStringBounds(aCharArr, 0, 1, frc);
+        TextLayout textLayout = new TextLayout(new String(aCharArr), font, frc);
+        Shape outline = textLayout.getOutline(null);
+
+        Rectangle2D bounds = outline.getBounds();
         BufferedImage image = new BufferedImage(
-                (int) bounds.getWidth(), (int)bounds.getHeight(), BufferedImage.TYPE_INT_ARGB
+                (int) bounds.getWidth() + 2, (int)bounds.getHeight() + 2, BufferedImage.TYPE_INT_ARGB
         );
 
         Graphics2D g = (Graphics2D) image.getGraphics();
-        FontMetrics fm   = g.getFontMetrics(font);
-        int y = fm.getAscent();
-
+        g.translate(-bounds.getX() + 1, -bounds.getY() + 1);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
         g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
         g.setComposite(AlphaComposite.Clear);
         g.fillRect(0, 0, image.getWidth(), image.getHeight());
         g.setComposite(AlphaComposite.Src);
+
         g.setColor(Color.BLACK);
-        g.setFont(font);
-        g.drawChars(aCharArr, 0, 1, 0, y);
-        g.drawRect(0, 0, image.getWidth() - 1, image.getHeight() - 1);
+        g.draw(outline);
+
         return image;
     }
 }
--- src/test/java/com/hyd/captcha/CaptchaGeneratorTest.java
+++ src/test/java/com/hyd/captcha/CaptchaGeneratorTest.java
@@ -1,29 +1,52 @@
 package com.hyd.captcha;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.util.List;
+import javax.swing.*;
 
 public class CaptchaGeneratorTest extends JFrame {
 
     public static void main(String[] args) {
         CaptchaGenerator captchaGenerator = new CaptchaGenerator();
-        BufferedImage bufferedImage = captchaGenerator.generate(100, 40, "Styles");
-        List<BufferedImage> charImages = CaptchaGenerator.getCharImages();
+        JLabel imageLabel = new JLabel();
+        JPanel charImagePanel = new JPanel();
+
+        Runnable displayImage = () -> {
+            BufferedImage bufferedImage = captchaGenerator.generate(250, 70, "captcha123456");
+            imageLabel.setIcon(new ImageIcon(bufferedImage));
+
+            List<BufferedImage> charImages = CaptchaGenerator.getCharImages();
+            charImagePanel.removeAll();
+            charImages.forEach(charImage -> charImagePanel.add(imageLabel(charImage)));
+        };
+
+        imageLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                displayImage.run();
+            }
+        });
 
         CaptchaGeneratorTest frame = new CaptchaGeneratorTest();
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
 
         frame.getContentPane().setLayout(new BorderLayout());
-        frame.getContentPane().add(imageLabel(bufferedImage), BorderLayout.CENTER);
+        frame.getContentPane().add(imageLabel, BorderLayout.CENTER);
 
-        JPanel charImagePanel = new JPanel();
         charImagePanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-        charImages.forEach(charImage -> charImagePanel.add(imageLabel(charImage)));
         frame.getContentPane().add(charImagePanel, BorderLayout.NORTH);
 
+        frame.setLocation(500, 300);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                displayImage.run();
+            }
+        });
         frame.setVisible(true);
     }
 
